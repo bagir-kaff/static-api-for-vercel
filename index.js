22b2c55519bb6b62735cef28f405dad0ff9b5451
bagir-kaff <trexhilang@gmail.com>
2024-12-28 16:28:59 +0330

    Good enough for testing

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const Joi = require('joi')
+const app = express()
+const sentences = require('./sentences.js')
+app.use(express.json())
+
+const PORT = process.env.PORT || 1000;
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+app.get('/',(req, res) => {
+    res.send(`i dont know, 
+        <br><a href="/sentences"> /sentences</a>
+        <br><a href="/sentences/1"> /sentences/:id id=1</a>
+        <br><a href="/sentences/sentence/random">/sentences/sentence/random</a>
+        <br><a href=""></a>
+        `)
+});
+
+app.get('/sentences', (req, res) => {
+    res.json(sentences)
+});
+
+app.post('/sentences', (req, res) => {
+    const schema = Joi.object({
+        sentence: Joi.string().min(6).required()
+    });
+
+    const result = schema.validate(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.message);
+        return;
+    }
+
+    console.log("new sentence is added ")
+    const sentence = {
+        id: sentences.length,
+        sentence: req.body.sentence,
+        length: req.body.sentence.length
+    }
+    sentences.push(sentence)
+    res.status(201).send(sentence)
+});
+
+app.get('/sentences/:id',(req, res) => {
+    const sentence = sentences.find(c => c.id === parseInt(req.params.id));
+    if (!sentence) res.status(404).send('The sentence with the given id was not found')//404
+    res.send(sentence)
+})
+
+app.get('/sentences/sentence/random', (erq, res) => {
+    const randomnum = getRandomInt(1,sentences.length-1)
+    // console.log(randomnum)
+    res.send(sentences[randomnum])
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
---
+++ sentences.js
@@ -0,0 +1,64 @@
+let sentences = [
+    {
+        id:0,
+        sentence: "this is just to make indexing easier by matching id and sentence index",
+        length:-1.
+    },
+    {
+        id: 1,
+        sentence: "A disciplined mind brings happiness. ~Buddha",
+        length: 44
+    },
+    {
+        id:2,
+        sentence: "You can stand tall without standing on someone. You can be a victor without having victims. ~Harriet Woods",
+        length: 106
+    },
+    {
+        id:3,
+        sentence:"Subdue your appetites, my dears, and you've conquered human nature. ~Charles Dickens",
+        length:67
+    },
+    {
+        id:4,
+        sentence:"Courage is rightly esteemed the first of human qualities... because it is the quality which guarantees all others. ~Winston Churchill",
+        length:114
+    },
+    {
+        id:5,
+        sentence:"The ideals which have lighted my way, and time after time have given me new courage to face life cheerfully, have been Kindness, Beauty, and Truth. The trite subjects of human efforts, possessions, outward success, luxury have always seemed to me contemptible. ~Albert Einstein",
+        length:260
+    },
+    {
+        id:6,
+        sentence:"Everything comes to him who hustles while he waits. ~Thomas Edison",
+        length:51
+    },
+    {
+        id:7,
+        sentence:"My religion consists of a humble admiration of the illimitable superior spirit who reveals himself in the slight details we are able to perceive with our frail and feeble mind. ~Albert Einstein",
+        length:176
+    },
+    {
+        id:8,
+        sentence:"Reading, after a certain age, diverts the mind too much from its creative pursuits. Any man who reads too much and uses his own brain too little falls into lazy habits of thinking. ~Albert Einstein",
+        length:180
+    },
+    {
+        id:9,
+        sentence:"It's only when the tide goes out that you discover who's been swimming naked. ~Warren Buffet",
+        length:77
+    },
+    {
+        id:10,
+        sentence:"There is no fear for one whose mind is not filled with desires. ~Buddha",
+        length:63
+    },
+    {
+        id: 11,
+        sentence:"They blame those who remain silent, they blame those who speak much, they blame those who speak in moderation. There is none in the world who is not blamed. ~Buddha",
+        length:156
+    }
+]
+
+module.exports = sentences;
