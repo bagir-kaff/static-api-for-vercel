c16e9c377f8a88e62b42e96f375f699a26f15e98
bagir-kaff <trexhilang@gmail.com>
2024-12-29 13:12:26 +0330

    finish last thing

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ function getRandomInt(min, max) {
 
 
 app.get('/',(req, res) => {
-    res.send(`endpoints: 
-        <br><a href="https://www.postman.com/buwagear/workspace/storing-sentence/documentation/39824000-671cebe3-3373-4a0b-96a5-656d9217464c">API DOCUMENTATION</a>
-        `)
+    res.send(`<br><a href="https://www.postman.com/buwagear/workspace/storing-sentence/documentation/39824000-671cebe3-3373-4a0b-96a5-656d9217464c">API DOCUMENTATION</a>`)
 });
 
 app.get('/sentences', (req, res) => {
